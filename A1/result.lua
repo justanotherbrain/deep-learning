a9f95cc91511a2a0720fbe8854087172e5a30724
DGarwin <downcrraft@yahoo.com>
2015-02-09 01:22:40 -0500

    bug fix and changed name to result.lua

--- A1/result.lua
+++ A1/result.lua
@@ -8,7 +8,7 @@ function setup()
   testData = {
      data = loaded.X:transpose(3,4),
      labels = loaded.y[1],
-     size = function() return 10 end
+     size = function() return 26032 end
   }
   model = torch.load('model.net')
   results = {{'samp#','target', 'pred'}}
