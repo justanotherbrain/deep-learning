5c44000a25feefb569d9677fd86cb5a805f47baf
Sam Royston <sfoxroyston@gmail.com>
2015-03-02 00:58:49 -0500

    preprocessing

--- A2/preprocess.lua
+++ A2/preprocess.lua
@@ -1,6 +1,7 @@
 require "torch"
 require "image"
 require "xlua"
+require "math"
 
 DEFAULT_DIR = "SurrogateData/"
 TRAIN_PREFIX = "surrogate_taindata_"
@@ -16,19 +17,31 @@ end
 function convert_to_YUV( t )
 
 	-- convert the compound tensor t, a set of images with expected dimension i x 3 x 36 x 36
+	
 	print ("converting to YUV -->")
-
 	for i=1,t:size() do
 		t[i] = image.rgb2yuv(t[i])
 		xlua.progress(i, trainData:size())
 	end
+	return t
+end
 
-function load_train_data()
+function load_data()
 	
-	-- load from file
+	-- load training data and labels from file
 	
 	data_filename, label_filename = train_decorate(1)
-	data = torch.load(data_filename)
+	data = convert_to_YUV(torch.load(data_filename))
 	labels = torch.load(label_filename)
 	return data, labels
+end
+
+function validation_split( data, fraction )
+	
+	-- separate data into train/test split
+
+	split_index = math.floor(data:size() * fraction)
+	train = data:sub(1,split_index)
+	test = data:sub(split_index, data:size())
+	
 end
